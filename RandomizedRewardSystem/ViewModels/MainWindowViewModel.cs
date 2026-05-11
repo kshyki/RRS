81cb75f0264143d13db3b8229c43f386c99da2ab
Kyrylo <kirillshtanko123@gmail.com>
2026-05-11 22:11:32 +0200

    Added properties for Score, Round, Multiplier

--- RandomizedRewardSystem/ViewModels/MainWindowViewModel.cs
+++ RandomizedRewardSystem/ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,26 @@
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private long _totalScore;
+    public long TotalScore
+    {
+        get => _totalScore;
+        set => this.RaiseAndSetIfChanged(ref _totalScore, value);
+    }
+
+    private int _currentRound;
+    public int CurrentRound
+    {
+        get => _currentRound;
+        set => this.RaiseAndSetIfChanged(ref _currentRound, value);
+    }
+
+    private int _roundMultiplier;
+    public int roundMultiplier
+    {
+        get => _roundMultiplier;
+        set => this.RaiseAndSetIfChanged(ref _roundMultiplier, value);
+    }
+
+    
 }
