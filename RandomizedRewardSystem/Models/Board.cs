4a0ee46aa0759c6011f3e572f13eff4e67ac4a3f
Kyrylo <kirillshtanko123@gmail.com>
2026-05-11 12:56:06 +0200

    Added search of the winning combination

--- RandomizedRewardSystem/Models/Board.cs
+++ RandomizedRewardSystem/Models/Board.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace RandomizedRewardSystem.Models
 {
     public class Board
@@ -5,8 +7,16 @@ namespace RandomizedRewardSystem.Models
         public const int Rows = 6;
         public const int Columns = 5;
 
+        public List<(int dr, int dc)> directions = new()
+        {
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1)
+        };
+        
         public Symbol[,] Grid;
-
+        public bool[,] visited;
         public Board()
         {
             Grid = new Symbol[Rows, Columns];
@@ -27,6 +37,63 @@ namespace RandomizedRewardSystem.Models
         public void SetSymbol(int row, int col, Symbol symbol)
         {
             Grid[row,col] = symbol;
+        }
+
+        private void Search(int r, int c, SymbolType targetType, bool[,] visited, List<(int r, int c)> group)
+        {
+
+            group.Add((r, c));
+            visited[r, c] = true;
+
+            foreach(var (dr, dc) in directions)
+            {
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if((nr >= 0) && (nr < Rows))
+                {
+                    if((nc >= 0) && (nc < Columns))
+                    {
+                        if(!visited[nr, nc])
+                        {
+                            if(Grid[nr, nc].Type == targetType)
+                            {
+                                Search(nr, nc, targetType, visited, group);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        public List<List<(int r, int c)>> FindWinningGroups()
+        {
+            var allGroups = new List<List<(int r, int c)>>();
+
+            bool [,] visited = new bool[Rows, Columns];
+
+            for(int r = 0; r < Rows; r++)
+            {
+                for(int c = 0; c < Columns; c++)
+                {
+                    if (!visited[r, c])
+                    {
+                        var CurrentGroup = new List<(int r, int c)>();
+                        
+                        if(Grid[r, c] != null)
+                        { 
+                            Search(r, c, Grid[r, c].Type, visited, CurrentGroup);   
+                        }
+
+                        if(CurrentGroup.Count >= 8)
+                        {
+                            allGroups.Add(CurrentGroup);
+                        }
+                    }
+                }
+            }
+
+            return allGroups;
         }
     }
 }
