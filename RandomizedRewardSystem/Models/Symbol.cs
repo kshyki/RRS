2ac76a8dbfddf820d100d799811f6fe1cc452179
Kyrylo <kirillshtanko123@gmail.com>
2026-06-04 11:29:50 +0200

    Added field ImagePath

--- RandomizedRewardSystem/Models/Symbol.cs
+++ RandomizedRewardSystem/Models/Symbol.cs
@@ -7,6 +7,8 @@ namespace RandomizedRewardSystem.Models
         public double Probability { get; set; }
         public bool IsMultiplier { get; set; }
 
+        public string ImagePath { get; set; }
+
         public Symbol(SymbolType type, int value, double probability, bool isMultiplier = false)
         {
             Type = type;
